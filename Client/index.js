869abf8941049460328ac3834a37a0a1d12c4553
Louis Nwamadi <louisdante9@gmial.com>
2021-01-19 21:46:18 +0100

    Use http://listjs.com  to show a list of rows, from some hard coded array

---
+++ Client/index.js
@@ -0,0 +1,11 @@
+let options = {
+  valueNames: ["name", "city"],
+  item: '<li><h3 class="name"></h3><p class="city"></p></li>',
+};
+
+let values = [
+  { name: "Jonny", city: "Stockholm" },
+  { name: "Jonas", city: "Berlin" },
+];
+
+export { options, values };
